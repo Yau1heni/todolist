395b8cc1278424a58f9ee69bb862a04342c3c6e0
Yau1heni <lodbrok9@gmail.com>
2022-12-30 18:00:04 +0300

    router

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -7,7 +7,9 @@ import {TaskType} from '../api/task-api';
 import {useAppSelector} from '../customHooks/hooks';
 import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar';
 import {RequestStatusType} from './app-reducer';
+import {Navigate, Route, Routes} from 'react-router-dom';
 import TodolistLists from '../features/TodolistsList/TodolistLists';
+import {Login} from '../features/Login/Login';
 
 export type TasksStateType = {
     [key: string]: Array<TaskType>
@@ -22,13 +24,17 @@ const App = () => {
             <ErrorSnackbar/>
             {status === 'loading' && <LinearProgress color="secondary"/>}
             <Container fixed>
-                <TodolistLists/>
+                <Routes>
+                    <Route path='/' element={<TodolistLists/>}/>
+                    <Route path='/login' element={<Login/>}/>
+
+                    <Route path='/404' element={<h2>404: PAGE NOT FOUND</h2>}/>
+                    <Route path='*' element={<Navigate to = '/404'/>}/>
+                </Routes>
             </Container>
         </div>
     );
 };
-
-
 
 
 export default App;
