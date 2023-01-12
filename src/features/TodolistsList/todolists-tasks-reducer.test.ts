c5f09f16581ae3002e56515cf0dc5e59a691d2f1
Yau1heni <lodbrok9@gmail.com>
2023-01-13 00:57:31 +0300

    refactor: test

---
+++ src/features/TodolistsList/todolists-tasks-reducer.test.ts
@@ -0,0 +1,19 @@
+import { tasksReducer } from "./Todolist/Task/tasks-reducer";
+import { TasksStateType } from "../../app/App";
+import { addTodolistAC, TodolistDomainType, todolistsReducer } from "./Todolist/todolists-reducer";
+
+test("ids should be equals", () => {
+  const startTasksState: TasksStateType = {};
+  const startTodolistsState: Array<TodolistDomainType> = [];
+
+  const action = addTodolistAC("new todolist");
+  const endTasksState = tasksReducer(startTasksState, action);
+  const endTodolistsState = todolistsReducer(startTodolistsState, action);
+
+  const keys = Object.keys(endTasksState);
+  const idFromTasks = keys[0];
+  const idFromTodolists = endTodolistsState[0].id;
+
+  expect(idFromTasks).toBe(action.todolistId);
+  expect(idFromTodolists).toBe(action.todolistId);
+});
